e6b372168e89747726610a037397de9b335b2315
anmolk11 <akcp.cpp@gmail.com>
2023-01-30 20:04:33 +0530

    modified rule.py to activate looping in over data set for repeated rule mining

--- rule.py
+++ rule.py
@@ -1,6 +1,10 @@
 from a import *
+import os
+
+threshold = 30
 
 def read_rule(dict):
+    dict = makeMonkey(dict)
     print("If ")
     for k,v in dict.items():
         if v[0] == 1:
@@ -9,15 +13,47 @@ def read_rule(dict):
             print(f"{mn} <= {col[k]} <= {mx} and ")
     print("Then Class = Positive")
 
-    
-    #print(f"Score :  {fitness(dict,0)}")
+def delRows(args,sign):
+    drop_ind = []
+    args = makeMonkey(args)
+    score = 0
+    index = df.index.tolist()
+    for i in index:
+        rule_satisfied = True
+        for k,v in args.items():
+            if v[0] == 1:
+                mn = min(v[1],v[2])
+                mx = max(v[1],v[2])
+
+                if df.loc[i][col[k]] < mn or df.loc[i][col[k]] > mx:
+                    rule_satisfied = False
+                    break
+        if rule_satisfied and df.loc[i]["Outcome"] == sign:
+            score += 1
+            drop_ind.append(i)
+
+    df.drop(drop_ind,axis = 0,inplace = True)
+    os.remove("diabetes.csv")
+    df.to_csv("diabetes.csv")
+    return score
 
 with open("final_rule.txt","r") as file:
-    a = [float(x.rstrip()) for x in file]
+    args = [float(x.rstrip()) for x in file]
 
-a = makeMonkey(a)
+#args = makeMonkey(args)
 
 print("\n ---------------------------------------------- \n")
-read_rule(a)
+
+print(f"Size of current Data set : {df.shape[0]}")
+read_rule(args)
+score = delRows(args,0)
+print(f"Hit score : {score}")
+
+if df.shape[0] > threshold:
+    os.system("g++ mainCode.cpp")
+    os.system("a")
+else:
+    print("\nEnd of the rule minig process\n")
+
 print("\n ---------------------------------------------- \n")
 
