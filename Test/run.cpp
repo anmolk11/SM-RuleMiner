7bb2a5bcde8f49b371f85071a2992b137786f852
anmolk11 <akcp.cpp@gmail.com>
2023-01-30 15:12:25 +0530

    updated the redo module and added some rule in run module but the csv file is not getting updated

--- Test/run.cpp
+++ Test/run.cpp
@@ -10,29 +10,36 @@ void write(vector<double> args){
     for(int i = 0;i < N;i++){
         wrt<<args[i]<<'\n';
     }
+    wrt.close();
 }
 
 vector<double> F(int id){
-    vector<double> args1 = {
-
+    vector<vector<double>> args={
+        {
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,19.27,96.28,0,0,0,0,0,0
+        },
+        {
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0.36,2.0,1,23,74
+        },
+        {
+        0,0,0,1,95,179,0,0,0,0,0,0,0,0,0,1,31.7,58.16,0,0,0,0,0,0
+        },
+        {
+        0,0,0,0,0,0,0,0,0,1,32,95,0,0,0,0,0,0,0,0,0,0,0,0
+        }
     };
-    vector<double> args2 = {
-
-    };
-    vector<double> args3 = {
-
-    };
-    vector<double> args4 = {
-
-    };
-
-    return args1;
+    return args[id]; 
 }
 
 
 int main(){
-    for(int i = 1;i <= 4;i++){
+    for(int i = 0;i < 4;i++){
         write(F(i));
         system("python redo.py");
+        rd.open("Flag.txt",ios::in);
+        string ok;
+        rd>>ok;
+        rd.close();
+        if(ok == "0") break;
     }
 }
